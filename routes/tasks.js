358b35e94cc187e44a8a2c535bca6a635c050433
Stray <strayfrozen@yahoo.com>
2022-12-20 15:14:56 -0600

    set up tasks.js in routes folder

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const {getAllTasks} = require('../controllers/tasks')
 
 
-router.route('/').get((req,res)=>{
-    res.send('all items')
-})
+
+router.route('/').get(getAllTasks)
 
 module.exports = router;
