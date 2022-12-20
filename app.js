59cccbbf0d66aa45a248ba453c978ced591d75de
Stray <strayfrozen@yahoo.com>
2022-12-20 13:53:49 -0600

    set up get route

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 
 
+/// routes
+app.get('/hello', (req, res)=>{
+    res.send('Task Manager App')
+})
+
+
 const port = 3000;
 
 app.listen(port, console.log(`Server is listening on port ${port}...`))
